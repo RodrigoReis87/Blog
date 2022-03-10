ef094c9a7105ab298a32093f28e66e7cd99401fe
Rodrigo Reis <rodrigo_reis_87@hotmail.com>
2022-03-10 20:58:59 -0300

    Configurando Autenticação

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,10 +1,11 @@
 ﻿using Blog.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers;
 
 [ApiController]
-public class AcountController : ControllerBase
+public class AccountController : ControllerBase
 {
     // Esse trecho de código é a mesma coisa que o parâmetro FromServices da linha 18
     // private readonly TokenService _tokenService;
@@ -14,11 +15,23 @@ public class AcountController : ControllerBase
     //     _tokenService = tokenService;
     // }
     
-    [HttpPost("v1 / login")]
+    [HttpPost("v1/login")]
     public IActionResult Login([FromServices]TokenService tokenService)
     {
         var token = tokenService.GenerateToken(null);
 
         return Ok(token);
     }
+    
+    [Authorize (Roles = "user")]
+    [HttpGet("v1/user")]
+    public IActionResult GetUser() => Ok(User.Identity.Name);
+    
+    [Authorize (Roles = "author")]
+    [HttpGet("v1/author")]
+    public IActionResult GetAuthor() => Ok(User.Identity.Name);
+    
+    [Authorize (Roles = "admins")]
+    [HttpGet("v1/admin")]
+    public IActionResult GetAdmin() => Ok(User.Identity.Name);
 }
